956d5e6fa27203b8d6e8edbd3cb444c22c9468d0
Alfonso Rios <manuelalfonso.rios.contractor@bbva.com>
2018-04-30 12:16:56 -0500

    refactor(*): Add brackets, add semi-colons, remove console.logs

--- davidh/root/index.js
+++ davidh/root/index.js
@@ -1,40 +1,36 @@
-'use strict'
+'use strict';
 // use this syntax on termina for choose de env: node index.js --env=enviroment
-const args = require('yargs').argv
+const args = require('yargs').argv;
 const fs = require('fs');
 
 // ************************************ funtions *********************************************
 // this is a asynchronous function that obtains a json data of enviroment
 const confEnv = (env) => {
-  const enviroment = require(`./env/${env}.json`) // get json
-  let template
+  const enviroment = require(`./env/${env}.json`);
+  let template;
 
   try {
-    template = fs.readFileSync('template.txt', 'utf8') // get template.txt
-  } catch(e) {
-    console.log('Error:', e)
+      template = fs.readFileSync('template.txt', 'utf8'); // get template.txt
+  } catch(error) {
+    console.log('Error:', error);
   }
-
-  // Here must be the template.output.txt algoritmo generator
-  console.log(enviroment)
-  console.log(template)
-}
+};
 
 //************************************** main program ****************************************
 // switch the arg env of terminal for make the new output file
 switch (args.env) {
   case 'dev':
-    console.log('configurando entorno de desarrollo...')
-    confEnv('dev')
+    console.log('configurando entorno de desarrollo...');
+    confEnv('dev');
     break;
   case 'qa':
-    console.log('configurando entorno de QA...')
-    confEnv('qa')
+    console.log('configurando entorno de QA...');
+    confEnv('qa');
     break;
   case 'prod':
-    console.log('configurando entorno de produccion...')
-    confEnv('prod')
+    console.log('configurando entorno de produccion...');
+    confEnv('prod');
     break;
   default:
-    console.log('no se eligió un entorno válido')
+    console.log('no se eligió un entorno válido');
 }
