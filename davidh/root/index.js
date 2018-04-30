970b8508242c865670c16a5aa5aa97503a35a78f
unknown <softtek@LSTKTR111055.stkbancomerans.com>
2018-04-30 00:04:40 -0500

    config enviroment advance

--- davidh/root/index.js
+++ davidh/root/index.js
@@ -1,22 +1,40 @@
-'use strict';
+'use strict'
 // use this syntax on termina for choose de env: node index.js --env=enviroment
-const args = require('yargs').argv;
+const args = require('yargs').argv
+const fs = require('fs');
 
-switch (args.env) {
-  case 'dev':
-    console.log('entorno de desarrollo')
-    break;
-  case 'qa':
-    console.log('entorno QA')
-    break;
-  case 'prod':
-    console.log('entorno de produccion')
-    break;
-  default:
-    console.log('no se eligio un env válido')
+// ************************************ funtions *********************************************
+// this is a asynchronous function that obtains a json data of enviroment
+const confEnv = (env) => {
+  const enviroment = require(`./env/${env}.json`) // get json
+  let template
 
+  try {
+    template = fs.readFileSync('template.txt', 'utf8') // get template.txt
+  } catch(e) {
+    console.log('Error:', e)
+  }
+
+  // Here must be the template.output.txt algoritmo generator
+  console.log(enviroment)
+  console.log(template)
 }
 
-
-
-//console.log('enviroment: ' + args.env);
+//************************************** main program ****************************************
+// switch the arg env of terminal for make the new output file
+switch (args.env) {
+  case 'dev':
+    console.log('configurando entorno de desarrollo...')
+    confEnv('dev')
+    break;
+  case 'qa':
+    console.log('configurando entorno de QA...')
+    confEnv('qa')
+    break;
+  case 'prod':
+    console.log('configurando entorno de produccion...')
+    confEnv('prod')
+    break;
+  default:
+    console.log('no se eligió un entorno válido')
+}
